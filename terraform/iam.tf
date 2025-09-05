72d0f78bc2b2ef49d07d9ae6e723d90a042c2a79
jidendiran-coder <joel.jade.networking@gmail.com>
2025-09-05 15:36:08 +0530

    terraform update

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -20,6 +20,6 @@ resource "aws_iam_role_policy_attachment" "cw_attach" {
 }
 
 resource "aws_iam_instance_profile" "ec2_profile" {
-  name = "mern-ec2-profile"
+  name = "jid-mern-ec2-profile"
   role = aws_iam_role.ec2_role.name
 }
